44c12e23e221de9b8787e16df6021f3753c9aba7
Akhmed <id.shakirov.akhmed@gmail.com>
2024-07-09 17:37:30 +0600

    fix

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,11 +1,8 @@
 <template>
     <h1 @click="logout">Good</h1>
-
-    <Icon icon="eye-dropper" />
 </template>
 
 <script setup lang="ts">
-import { Icon } from '@ui/index'
 import { logout } from '@/entities/authorization'
 </script>
 
